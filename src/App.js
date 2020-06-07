8bda01a375b570259e4e2784b13a7c1388935718
Pr0sper0 <gestor009@gmail.com>
2020-06-07 18:24:24 +0300

    photo loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 import logo from './logo.svg';
 import './styles/main.css';
 import { BrowserRouter as Router, HashRouter, Switch, Route } from 'react-router-dom';
@@ -9,15 +9,28 @@ import Experience from './components/Experience';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 
-
 import { DualRing } from 'react-awesome-spinners';
+import Background from "./img/background/P50520-202802-min.jpg";
+
+const HomePage = lazy(() => import("./components/Home"));
 
 function App() {
 
   const [loading, toggleLoading] = useState(true);
 
+  let image = {};
+
+  const imageLoad = async () => {
+    const response = await fetch(`./img/background/P50520-202802-min.jpg`);
+    image = await response.body;
+    //console.log(image);
+    //setTimeout(() => toggleLoading(false), 1000)
+    toggleLoading(false);
+  }
+
   useEffect(() => {
-    setTimeout(() => toggleLoading(false), 2000)
+    imageLoad();
+
   }, [])
 
   return (
@@ -27,7 +40,9 @@ function App() {
         <HashRouter>
           <Navigation />
           <Switch>
+
             <Route exact path="/home" component={Home} />
+
             <Route exact path="/about" component={About} />
             <Route exact path="/experience" component={Experience} />
             <Route exact path="/portfolio" component={Portfolio} />
